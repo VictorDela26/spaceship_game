a848b3956665e1293eb5218fc1221b4984aa2bed
Victordela26 <victor.dela26@gmail.com>
2025-06-12 01:44:33 -0500

    Moving W/S

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def main():
     pygame.display.set_caption("My Game")
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     clock = pygame.time.Clock()
+    updatables = [player]
+    drawables = [player]
     dt = 0
 
     while True:
@@ -19,10 +21,14 @@ def main():
                 return
             
         screen.fill((0, 0, 0))
-        player.draw(screen)
-        player.update(dt)
-        pygame.display.flip()
 
+        for obj in drawables:
+            obj.draw(screen)
+        
+        for obj in updatables:
+            obj.update(dt)
+        
+        pygame.display.flip()
         dt = clock.tick(60) / 1000
     
 if __name__ == "__main__":
